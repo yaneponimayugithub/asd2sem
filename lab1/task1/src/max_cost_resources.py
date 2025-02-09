407e7987225ccfc5520f939431a6081c6caed8d0
Ilkham Gashimov <ilhamqasimov46@gmail.com>
2025-02-09 12:02:35 +0300

    finished lab 1 task 1

---
+++ lab1/task1/src/max_cost_resources.py
@@ -0,0 +1,37 @@
+import os
+import lab1.utils as utils
+
+input_path, output_path = utils.get_file_paths(os.path.abspath(__file__))
+
+
+def max_cost_items(n, W, items):
+    items.sort(key=lambda item_params: float('inf') if item_params[1] == 0 else item_params[0] / item_params[1], reverse=True)   # сортировка по убыванию ценности, если вес = 0, то присвоим inf
+
+    finish_cost = 0.0  # окончательная стоимость
+    for value, weight in items:
+        if W == 0:
+            break
+        if weight <= W:
+            # Если весь предмет помещается в рюкзак
+            W -= weight
+            finish_cost += value
+            print(W,finish_cost)
+        else:
+            # Если можем взять только часть предмета
+            finish_cost += value * (W / weight)
+            W = 0
+
+    return finish_cost
+
+if __name__ == '__main__':
+    @utils.measure_time_and_memory
+    def task():
+        result = []
+        lines = utils.read_input(input_path)
+        n, W = map(int, lines[0].split())
+        items = [list(map(int, line.split())) for line in lines[1:]]
+        result = max_cost_items(n, W, items)
+        utils.write_output(output_path, result)
+
+
+    task()
