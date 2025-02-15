60c6cd6fd57a286d3ffa225cbbc546ffb0ade55c
Ilkham Gashimov <ilhamqasimov46@gmail.com>
2025-02-15 14:33:26 +0300

    изменение сложности при худших обств с n**2 до n

--- lab1/task2/src/gas_stations.py
+++ lab1/task2/src/gas_stations.py
@@ -4,34 +4,34 @@ import lab1.utils as utils
 input_path, output_path = utils.get_file_paths(os.path.abspath(__file__))
 
 def count_gas_stations(distance: int, distance_on_full_fuel: int, count_gas_stations: int, dist_gas_stations: list):
-    dist_gas_stations.insert(0, 0)  # добавляем 0 чтобы сравнивать с начальной точки
+    dist_gas_stations.insert(0, 0)  # добавляем 0 чтобы сравнивать с начальной точкой
 
     if distance <= distance_on_full_fuel: return 0  # если можно доехать на полном баке без заправки
 
     if dist_gas_stations[-1] + distance_on_full_fuel < distance: return -1  # проверка на возможность проезда назначенной пути
 
-    stations = []  # посещенные станции
+    numRefills = 0  # количество заправок
     current_gas_station = 0  # начальная станция
 
     while current_gas_station < count_gas_stations:
-        next_gas_station = current_gas_station
-        # ищем самую дальнюю заправку до которой можно доехать
-        for current_idx in range(current_gas_station + 1, count_gas_stations + 1):
-            if dist_gas_stations[current_idx] - dist_gas_stations[current_gas_station] <= distance_on_full_fuel:
-                next_gas_station = current_idx  # обновляем индекс заправки
-            else:
-                break  # если не хватит бенз, то прерываем цикл и изменение след.станции
+        last_gas_station = current_gas_station
+        # ищем самую дальнюю заправку, до которой можно доехать
+        # учитывается так же расстояние между заправками на полном баке
+        while current_gas_station < count_gas_stations and dist_gas_stations[current_gas_station + 1] - dist_gas_stations[last_gas_station] <= distance_on_full_fuel:
+            current_gas_station += 1
 
-        # проверка на то, что ближайшая заправка не дальше чем distance_on_full_fuel
-        if next_gas_station == current_gas_station: return -1
+        # если не можем найти заправку, до которой можно доехать
+        if current_gas_station == last_gas_station:
+            return -1
 
-        stations.append(dist_gas_stations[next_gas_station])  # добавим заправку в список посещенныъ
-        current_gas_station = next_gas_station  # обновляем текущую заправку
+        numRefills += 1  # увеличиваем количество заправок
 
-        # проверка хватит ли бенз чтобы доехать без заправок
-        if dist_gas_stations[current_gas_station] + distance_on_full_fuel >= distance: break
+        # если на текущей заправке можем доехать до цели
+        if dist_gas_stations[current_gas_station] + distance_on_full_fuel >= distance:
+            break
 
-    return len(stations)
+    return numRefills
+
 
 if __name__ == '__main__':
     @utils.measure_time_and_memory
